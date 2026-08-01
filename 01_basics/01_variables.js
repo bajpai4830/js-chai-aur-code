888c200438ef869b761e17bfdcfa7ad715240597
Ayush Bajpai <ayushbajpaiid2@gmail.com>
2026-08-01 08:35:41 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=144553
+let accountEmail="ayush@google.com"
+var accountPassword="12345"
+accountCity = "Lucknow"
+accountEmail="ab@.com"
+// accountId=2 //not allowed
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountPassword]);
